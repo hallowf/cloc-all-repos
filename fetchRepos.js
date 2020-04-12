af55e9effd6eefa0cfb4778b02742551f67387f0
hallowf <alexandre.cortegaca@gmail.com>
2020-04-12 15:42:56 +0100

    Added function to fetch repos from gitlab API

--- fetchRepos.js
+++ fetchRepos.js
@@ -3,18 +3,41 @@ const axios = require('axios')
 module.exports = {
   fetchGithub: function(username) {
     return fetchGithub(username)
+  },
+  fetchGitlab: function(username) {
+    return fetchGitlab(username)
   }
 }
 
 async function fetchGithub(username) {
   let repos = []
 
-  await axios.get('https://api.github.com/users/' + username + '/repos')
-    .then(res =>{
+  await axios.get(`https://api.github.com/users/${username}/repos`)
+    .then(res => {
       data = res.data
       data.forEach((item, i) => {
         repoData = {
           url: item.html_url,
+          name: item.name
+        }
+        repos.push(repoData)
+      });
+    })
+    .catch(err => {
+      console.error(err)
+    })
+
+  return repos
+}
+
+async function fetchGitlab(username) {
+  let repos = []
+  await axios.get(`https://gitlab.com/api/v4/users/${username}/projects`)
+    .then(res => {
+      data = res.data
+      data.forEach((item, i) => {
+        repoData = {
+          url: item.http_url_to_repo,
           name: item.name
         }
         repos.push(repoData)
