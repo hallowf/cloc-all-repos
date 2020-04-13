77261efd42944b6f1dde4d3288e492eb4918d669
hallowf <alexandre.cortegaca@gmail.com>
2020-04-13 21:12:47 +0100

    Added parameter to keep individual reports

--- cli.js
+++ cli.js
@@ -26,13 +26,18 @@ const argv = yargs
     describe: 'Logs full error stack to console',
     type: 'boolean'
   })
+  .option('keep-individual-reports', {
+    alias: 'kir',
+    describe: 'Keeps each individual report',
+    type: 'boolean',
+    default: false
+  })
   .demandOption(['platform', 'username'], 'Please provide platform and username arguments to run the program')
   .help()
   .argv
 
 const platform = argv.p.toLowerCase()
 const user = argv.u
-const rep = argv.rep
 
 async function fetchAndCount() {
   let repos = []
@@ -54,17 +59,28 @@ async function fetchAndCount() {
 }
 
 function cloneAndCount(repoList) {
+  //  create temporary directory
   console.log('Creating temp directory')
   shell.mkdir('temp')
+  // if keep individual reports create report directory
+  if (argv.kir) {
+    console.log('Creating reports directory')
+    shell.mkdir('reports')
+  }
   shell.cd('temp')
   let reportFiles = []
   repoList.forEach((item, i) => {
+    // clone repo
     shell.exec('git clone ' + item.url)
-    // count lines and make a report file
+    // if keep individual reports create reports directory
+    let reportName = `${item.name}.txt`
+    if (argv.kir) {reportName = `../reports/${item.name}.txt`}
+    // count lines make a report file and append it to reportFiles
     console.log("Creating report")
-    shell.exec(`cloc ${item.name} --report-file=${item.name}.txt`, {silent: true})
-    reportFiles.push(`${item.name}.txt`)
-    if (rep) {
+    shell.exec(`cloc ${item.name} --report-file=${reportName}`, {silent: true})
+    reportFiles.push(reportName)
+    // reamoves each repo after report
+    if (argv.rep) {
       console.log(`Removing ${item.name}`)
       shell.rm('-rf', item.name)
     }
@@ -73,7 +89,8 @@ function cloneAndCount(repoList) {
   allFilesString = reportFiles.join(' ')
 
   console.log('Joining reports, this might take a while')
-  // Join all reports
+  // Join all reports before changing directory
+  // to preserve the location of individual reports
   shell.exec(`cloc --sum-reports --report_file=../${platform}${user} ${allFilesString}`)
   shell.cd('..')
   console.log('removing temp directory')
